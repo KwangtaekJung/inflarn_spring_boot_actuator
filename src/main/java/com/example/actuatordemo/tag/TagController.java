414e170cf0508f1707d375042a84f56530b4a2e9
KwangtaekJung <kt79.jung@gmail.com>
2023-10-02 20:25:22 +0900

    metrics-@Counted with extraTags

--- src/main/java/com/example/actuatordemo/tag/TagController.java
+++ src/main/java/com/example/actuatordemo/tag/TagController.java
@@ -1,5 +1,6 @@
 package com.example.actuatordemo.tag;
 
+import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,20 @@ public class TagController {
     @GetMapping("/pop")
     public String pop() {
         myQueueManagerWithTag.pop();
+
+        return "ok";
+    }
+
+    @Counted(value = "my.counted.tag", extraTags = {"type1", "value1", "type2", "value2"})
+    @GetMapping("/counted/push")
+    public String counted_push() {
+
+        return "ok";
+    }
+
+    @Counted(value = "my.counted.tag", extraTags = {"type3", "value3", "type4", "value4"})
+    @GetMapping("/counted/pop")
+    public String counted_pop() {
 
         return "ok";
     }
